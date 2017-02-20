d1d81220e3c25e92f1149ccb90673f488ed4ca7d
Joe <jozsef.gulyas@ubs.com>
2017-02-20 09:35:52 -0500

    housekeeping

--- data-providers/fileProvider.js
+++ data-providers/fileProvider.js
@@ -1,7 +1,7 @@
 var Q = require('q');
 var exports = module.exports = {};
-var jan_30 = require('../ea_2017_01_30.json');
-var feb_10 = require('../ea_2017_02_10.json');
+var jan_30 = require('../resources/ea_2017_01_30.json');
+var feb_10 = require('../resources/ea_2017_02_10.json');
 
 exports.getAllCars = function() {
   var deferred = Q.defer();
